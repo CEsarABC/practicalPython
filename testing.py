cc38d7c5e7c9ee5b4a5e9769b17807bfec184949
Cesar Bernal <cesarbworkmail@gmail.com>
2018-11-12 10:46:30 +0000

    working on testing

---
+++ testing.py
@@ -0,0 +1,36 @@
+
+
+
+def keeping_name():
+    names = []
+    for i in range(0, 5):
+        guest_name = str(input('we will keep yot name in a list: '))
+        names.append(guest_name)
+        print(names)
+        i += 1
+    
+keeping_name()
+
+def test():
+    S=input('')
+    L=[]
+    while S!=4:
+        L.append(S)
+        if S==4:
+            break
+    print(L)
+    
+def keeping_name1():
+    our_list = [] # create empty list
+    for i in range(0, 5):
+ 
+        guest_name = int(input('Enter first name: '))
+     
+        our_list.append(guest_name)
+
+    print(our_list)
+    
+#keeping_name1()
+
+
+
