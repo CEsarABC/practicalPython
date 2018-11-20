51143107f07aefcc179337e0935480fcdd13693f
cesar bernal <cesarbworkmail@gmail.com>
2018-11-20 10:45:58 +0000

    core code working after tutor session, code re arranged

--- run3.py
+++ run3.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,session,redirect,url_for,request
 from flask_wtf import FlaskForm
-from wtforms import (StringField,BooleanField,DateTimeField,RadioField,SelectField,TextField,TextAreaField,SubmitField)
+from wtforms import (StringField,BooleanField,SelectField,TextField,TextAreaField,SubmitField)
+
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -49,7 +50,7 @@ class RiddleForm(FlaskForm):
     answer = StringField('Please insert your Answer: ', validators=[DataRequired()])
     submit = SubmitField('Ok')
 
-############ first definition, first validation, redirecting to next game
+############ first definition, first validation, redirecting to next game, user name
 
 @app.route('/', methods=['GET','POST'])
 def test_root():
@@ -64,29 +65,33 @@ def test_root():
 @app.route('/show', methods=['GET','POST'])
 def test_index():
     form = RiddleForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+    ##### issue not comparing answer comparing wrong riddle
+            session['userInput'] = form.answer.data
 
-    if 'counter' in session:
-        session['counter'] = session.get('counter') + 1
-        #need to add if session[counter]=14 end of the quiz
-        #redirect results
+            for riddle, answer in riddles_list:
+                # test print(session['riddle'], answer, session['userInput'])
+                if session['riddle'] == riddle and session['userInput'] == answer:
+                    session['score'] = session.get('score') + 1
+                # else:
+                #     return(session['riddle'])
+            return redirect(url_for('test_index'))
     else:
-        session['counter'] = 0
-        #session['riddle']=riddles[0]
-        session['score'] = 0
-#return 'counter is: {}, the riddle is: {}'.format(session.get('counter'), session.get('riddle'))
-    session['riddle']=riddles[session.get('counter')]
+        if 'counter' in session:
+            session['counter'] = session.get('counter') + 1
+            #need to add if session[counter]=14 end of the quiz
+            #redirect results
+        else:
+            session['counter'] = 0
+            #session['riddle']=riddles[0]
+            session['score'] = 0
+        #test print(session['counter'])
+    #return 'counter is: {}, the riddle is: {}'.format(session.get('counter'), session.get('riddle'))
+        session['riddle']=riddles[session.get('counter')]
 
-
-    if form.validate_on_submit():
-##### issue not comparing answer comparing wrong riddle
-        userInput = form.answer.data
-
-        for riddle, answer in riddles_list:
-            if session['riddle'] == riddle and userInput == answer:
-                session['score'] = session.get('score') + 1
-            # else:
-            #     return(session['riddle'])
         #return redirect(url_for('test_index'))
+
 
     return render_template('test.html', form = form)
 
