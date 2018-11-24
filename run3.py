3d2136b88a6be00b55e1716efff7c14c6e62b209
cesar bernal <cesarbworkmail@gmail.com>
2018-11-24 12:46:32 +0000

    I stop the use of wt forms for normal flask, more clean and less code

--- run3.py
+++ run3.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,session,redirect,url_for,request
-from flask_wtf import FlaskForm
-from wtforms import (StringField,BooleanField,SelectField,TextField,TextAreaField,SubmitField)
+import string
+
 
 from wtforms.validators import DataRequired
 
@@ -25,7 +25,7 @@ answers = []
 
 ########### organising the info in separate lists
 
-with open('data/riddles1.txt', 'r') as file:
+with open('data/riddles2.txt', 'r') as file:
     lines = file.read().splitlines()
 
 for i, text in enumerate(lines):
@@ -40,46 +40,35 @@ number_of_riddles = len(riddles)
 riddles_and_answers = zip(riddles, answers)
 riddles_list = list(riddles_and_answers)
 #print(riddles_list)
-############## creating forms for validation
 
-class InfoForm(FlaskForm):
-    userName = StringField('Please insert your Nickname: ', validators=[DataRequired()])
-    submit = SubmitField('Ok')
-
-class RiddleForm(FlaskForm):
-    answer = StringField('Please insert your Answer: ', validators=[DataRequired()])
-    submit = SubmitField('Ok')
 
 ############ first definition, first validation, redirecting to next game, user name
 
 @app.route('/', methods=['GET','POST'])
 def test_root():
-    form = InfoForm()
-    if form.validate_on_submit():
-        session['userName'] = form.userName.data
-        return redirect(url_for('test_index'))
-    return render_template('index.html',form=form)
+    if request.method == 'POST':
+        session['userName'] = request.form['UsernameInput']
+        return redirect(url_for('run_index'))
+    return render_template('index.html')
 
 ############ second definition accessing riddles and taking inputs
 
 @app.route('/show', methods=['GET','POST'])
-def test_index():
-    form = RiddleForm()
+def run_index():
     if request.method == "POST":
-        if form.validate_on_submit():
-    ##### issue not comparing answer comparing wrong riddle
-            session['userInput'] = form.answer.data
+        session['userInput'] = request.form['answerInput']
+        userAnswerData = session['userInput']
+        for riddle, answer in riddles_list:
+            if session['riddle'] == riddle and session['userInput'].lower() == answer:
+                session['score'] = session.get('score') + 1
 
-            for riddle, answer in riddles_list:
-                # test print(session['riddle'], answer, session['userInput'])
-                if session['riddle'] == riddle and session['userInput'] == answer:
-                    session['score'] = session.get('score') + 1
-                # else:
-                #     return(session['riddle'])
-            return redirect(url_for('test_index'))
+        return redirect(url_for('run_index'))
     else:
         if 'counter' in session:
             session['counter'] = session.get('counter') + 1
+            if session.get('counter') == 4 :
+                return render_template('results.html')
+
             #need to add if session[counter]=14 end of the quiz
             #redirect results
         else:
@@ -87,13 +76,17 @@ def test_index():
             #session['riddle']=riddles[0]
             session['score'] = 0
         #test print(session['counter'])
-    #return 'counter is: {}, the riddle is: {}'.format(session.get('counter'), session.get('riddle'))
+    # test return 'counter is: {}, the riddle is: {}'.format(session.get('counter'), session.get('riddle'))
         session['riddle']=riddles[session.get('counter')]
 
-        #return redirect(url_for('test_index'))
+        #return redirect(url_for('run_index'))
 
+    return render_template('test.html')
 
-    return render_template('test.html', form = form)
+@app.route('/results')
+def results():
+    # need to clear the session for the next player
+    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
