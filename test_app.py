a14ea076fe8320a4c4a031224168a4ecb318f6a9
Cesar Bernal <cesarbworkmail@gmail.com>
2018-12-27 17:29:23 +0000

    new changes

--- test_app.py
+++ test_app.py
@@ -80,6 +80,7 @@ class TestSession_userName(TestCase):
             with c.session_transaction() as sess:
                 sess['userName'] = True
             resp = c.get('/')
+        print(resp.data)
         self.assertEqual(b'<!DOCTYPE html>\n<html>\n  <head>\n    <m[3569 chars]tml>', resp.data)
 
 
